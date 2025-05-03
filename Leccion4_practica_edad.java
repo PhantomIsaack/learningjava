d60a52c73439c233248a82115887d0a8a1a664c1
PhantomIsaack <puedesermanolo@gmail.com>
2025-05-03 15:39:09 -0600

    condiciones

---
+++ Leccion4_practica_edad.java
@@ -0,0 +1,20 @@
+public class Leccion4_practica_edad {
+	public static void main(String[] args){
+	//variable
+		int edad = 80;
+	/*condiciones condicion inicial empezando con el mayor
+	 para que se automatice la ultima condicion*/
+	if (edad>=18){
+		System.out.println("Eres adulto");
+		}
+		else if  (edad>=13 && edad<=17){
+		System.out.println("Eres adolecente");
+		}
+		else {
+		System.out.println ("Eres niño");
+		}
+
+
+	}
+
+}
