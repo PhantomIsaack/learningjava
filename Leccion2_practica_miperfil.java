3842d4c51a1d29a22794e51be7b314b16ba83c04
PhantomIsaack <puedesermanolo@gmail.com>
2025-05-03 15:37:25 -0600

    Tipos de datos

---
+++ Leccion2_practica_miperfil.java
@@ -0,0 +1,18 @@
+public class Leccion2_practica_miperfil{
+	public static void main (String [] args){
+		String nombre = "Alejandro";
+		int edad = 22;
+		String altura = "11 metros";
+		char inicial = 'A';
+		boolean meGustaJava= true;
+
+		System.out.println("Mi nombre es: "+nombre);
+		System.out.println("Mi edad es: "+edad);
+		System.out.println("Mido: "+altura);
+		System.out.println("Mi inicial es: "+inicial);
+		System.out.println("me gusta java"+meGustaJava);
+
+	}
+
+
+}
