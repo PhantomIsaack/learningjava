43fe628534051157c35d49697218e8912d7020b9
PhantomIsaack <puedesermanolo@gmail.com>
2025-05-03 15:40:38 -0600

    listas y su recorrido

---
+++ Leccion6_practica_frutas.java
@@ -0,0 +1,12 @@
+public class Leccion6_practica_frutas {
+	public static void main(String[] args){
+		//declarar la variable String
+		String[] nombres={"Phantom", "Manolo", "Alejadnro", "Isaaac", "Lobo"};
+		//ciclo para recorrer toda
+		for(int i=0; i<nombres.length; i++){
+			System.out.println(nombres[i]);
+			}
+
+
+	}
+}
