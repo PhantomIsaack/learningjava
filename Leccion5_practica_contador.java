aee25ef8a533bd5b672c853b05699510d99df8cb
PhantomIsaack <puedesermanolo@gmail.com>
2025-05-03 15:39:50 -0600

    bucle while

---
+++ Leccion5_practica_contador.java
@@ -0,0 +1,16 @@
+public class Leccion5_practica_contador{
+	public static void main(String[] args){
+
+	int n=10;
+
+//conteo hacia atras
+	while(n>=0){
+		System.out.println("n vale: "+n);
+		if (n==0){
+			System.out.println("Despegue");
+			}
+		n--;
+
+		}
+	}
+}
