12cb4a30e138f3b3025172b0d252287c6d8d5b67
PhantomIsaack <puedesermanolo@gmail.com>
2025-05-03 15:38:18 -0600

    matematicas basicas

---
+++ Leccion3_practica_calculadora.java
@@ -0,0 +1,16 @@
+public class Leccion3_practica_calculadora{
+	public static void main(String[] args){
+//delcaracion de variables
+		int a=9;
+		int b=3;
+//uso de operadores
+		System.out.println("La suma es: "+(a+b));
+		System.out.println("La resta es: "+(a-b));
+		System.out.println("La multiplicacion es; "+(a*b));
+		System.out.println("La division es: "+(a/b));
+		System.out.println("El modulo es: "+(a%b));
+		System.out.println("'a' es mayor que 'b': "+(a>b));
+
+
+		}
+}
